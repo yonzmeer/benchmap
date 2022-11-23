f7ffb1dfdcf41587bc6fa22ef218fe01bb5fa37e
yonzmeer <meersonj@gmail.com>
2022-11-23 20:10:27 +0200

    first commit

---
+++ apps/benchmap/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'benchmap-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'benchmap';
+}
