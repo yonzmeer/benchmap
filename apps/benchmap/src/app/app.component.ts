6a27fe2d1feecccf59fb406192e3672fa2ea90bf
yonzmeer <meersonj@gmail.com>
2022-11-23 20:14:03 +0200

    remove initial stuff

--- apps/benchmap/src/app/app.component.ts
+++ apps/benchmap/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'benchmap-root',
+  selector: 'bnp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'benchmap';
-}
+export class AppComponent {}
